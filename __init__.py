11bd7b11b114b08c6120b1b52a3fbb21e7fe3105
Numerlor <Numerlor@gmail.com>
2022-01-30 19:06:50 +0100

    patch in button into the correct parent
    As we're looking for the if now instead of the block inside of it, the depth is
    one less

--- __init__.py
+++ __init__.py
@@ -32,8 +32,8 @@ def add_button():
     for wrapped_node in walk_sl_ast(NodeWrapper(screen_to_patch, None, 0)):
         if isinstance(wrapped_node.node, (slast.SLIf, slast.SLShowIf)):
             if any(u"_in_replay" in entry for entry in wrapped_node.node.entries):
-                wrapped_node.parent.parent.node.children.insert(
-                    wrapped_node.parent.pos_in_parent - 1, patch_screen
+                wrapped_node.parent.node.children.insert(
+                    wrapped_node.pos_in_parent - 1, patch_screen
                 )
                 break
     else:
