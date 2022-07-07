75f0538318c191470702e5e13121af82dbfaf607
Numerlor <Numerlor@gmail.com>
2022-07-07 22:18:01 +0200

    use double quotes
    the single quotes aren't necessary when we're using !r for quoting

--- script_jump/utils.py
+++ script_jump/utils.py
@@ -109,7 +109,7 @@ escape_renpy_formatting = partial(re.compile(r"\{+|\[+").sub, _sub_brackets_with
 def _scene_template(__wrapped_node):
     # type: (NodeWrapper[renpy.ast.Scene]) -> t.Text
     return (
-        'find_scene(find_label({wrapped_node.label_name!r}), name={joined_name!r}, layer={wrapped_node.node.layer!r})'
+        "find_scene(find_label({wrapped_node.label_name!r}), name={joined_name!r}, layer={wrapped_node.node.layer!r})"
     ).format(
         joined_name=" ".join(__wrapped_node.node.imspec[0]),
         wrapped_node=__wrapped_node,
@@ -118,7 +118,7 @@ def _scene_template(__wrapped_node):
 
 def _show_template(__wrapped_node):
     # type: (NodeWrapper[renpy.ast.Show]) -> t.Text
-    return 'find_show(find_label({wrapped_node.label_name!r}), {joined_name!r})'.format(
+    return "find_show(find_label({wrapped_node.label_name!r}), {joined_name!r})".format(
         joined_name=" ".join(__wrapped_node.node.imspec[0]),
         wrapped_node=__wrapped_node,
     )
@@ -127,7 +127,7 @@ def _show_template(__wrapped_node):
 def _user_statement_template(__wrapped_node):
     # type: (NodeWrapper[renpy.ast.UserStatement]) -> t.Text
     return (
-        'find_user_statement(find_label({wrapped_node.label_name!r}), {joined_name!r}, {wrapped_node.node.parsed})'
+        "find_user_statement(find_label({wrapped_node.label_name!r}), {joined_name!r}, {wrapped_node.node.parsed})"
     ).format(
         joined_name=" ".join(__wrapped_node.node.parsed[0]),
         wrapped_node=__wrapped_node,
@@ -145,13 +145,13 @@ def get_node_find_string(wrapped_node):
 
 # dict[type[T], t.Callable[[NodeWrapper[T]], t.Text]]
 _node_find_templates = {
-    renpy.ast.Say: 'find_say(find_label({0.label_name!r}), what={0.node.what!r}, who={0.node.who!r})'.format,
-    renpy.ast.Label: 'find_label(find_label({0.label_name!r}), {0.node.name!r})'.format,
-    renpy.ast.Jump: 'find_jump(find_label({0.label_name!r}), {0.node.target!r})'.format,
-    renpy.ast.Return: 'find_return(find_label({0.label_name!r}))'.format,
-    renpy.ast.Menu: 'find_menu(find_label({0.label_name!r}))'.format,
-    renpy.ast.Python: 'find_code(find_label({0.label_name!r}), {{}})'.format,
-    renpy.ast.Call: 'find_call(find_label({0.label_name!r}), {0.node.label!r})'.format,
+    renpy.ast.Say: "find_say(find_label({0.label_name!r}), what={0.node.what!r}, who={0.node.who!r})".format,
+    renpy.ast.Label: "find_label(find_label({0.label_name!r}), {0.node.name!r})".format,
+    renpy.ast.Jump: "find_jump(find_label({0.label_name!r}), {0.node.target!r})".format,
+    renpy.ast.Return: "find_return(find_label({0.label_name!r}))".format,
+    renpy.ast.Menu: "find_menu(find_label({0.label_name!r}))".format,
+    renpy.ast.Python: "find_code(find_label({0.label_name!r}), {{}})".format,
+    renpy.ast.Call: "find_call(find_label({0.label_name!r}), {0.node.label!r})".format,
     renpy.ast.Scene: _scene_template,
     renpy.ast.Show: _show_template,
     renpy.ast.UserStatement: _user_statement_template,
